a0e10bc0e0573ea56cb3b09618bf13fe9dcfbc20
vfor4 <trandangvu2708@gmail.com>
2023-11-23 22:52:37 +0700

    day12

--- microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
+++ microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -8,9 +8,11 @@ import io.swagger.v3.oas.models.info.License;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
@@ -62,4 +64,9 @@ public class ProductCompositeServiceApplication {
         return Schedulers.newBoundedElastic(poolSize, taskQueue, "publish-event-pool");
     }
 
+    @Bean
+    @LoadBalanced
+    WebClient.Builder webClientBuilder(){
+        return WebClient.builder();
+    }
 }
