6e4a840386d4947422861d691da264df198f6b40
Phuoc Nguyen <thanhphuoc01012001@gmail.com>
2026-01-17 23:11:19 +0700

    feat: using ConfigurationProperties in course composite config

--- microservices/course-composite-service/src/main/java/com/vitamax/course_composite_service/config/UtilAutoConfig.java
+++ microservices/course-composite-service/src/main/java/com/vitamax/course_composite_service/config/UtilAutoConfig.java
@@ -1,7 +1,5 @@
 package com.vitamax.course_composite_service.config;
 
-import com.vitamax.util.ServiceUtil;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,21 +7,6 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class UtilAutoConfig {
-    @Bean
-    public ServiceUtil courseServiceUtil(@Value("${services.course.port}") String port) {
-        return new ServiceUtil(port);
-    }
-
-    @Bean
-    public ServiceUtil recommendationServiceUtil(@Value("${services.recommendation.port}") String port) {
-        return new ServiceUtil(port);
-    }
-
-    @Bean
-    public ServiceUtil reviewServiceUtil(@Value("${services.review.port}") String port) {
-        return new ServiceUtil(port);
-    }
-
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
