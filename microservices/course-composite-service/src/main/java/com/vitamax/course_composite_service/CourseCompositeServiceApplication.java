05ae614a0067a0dcab3afaa26e8335e165a0ed5d
Phuoc Nguyen <pnguyen@blueway.fr>
2026-03-09 11:49:03 +0700

    feat: update swagger with @Securityxxxx

--- microservices/course-composite-service/src/main/java/com/vitamax/course_composite_service/CourseCompositeServiceApplication.java
+++ microservices/course-composite-service/src/main/java/com/vitamax/course_composite_service/CourseCompositeServiceApplication.java
@@ -1,12 +1,13 @@
 package com.vitamax.course_composite_service;
 
+import com.vitamax.api.config.SwaggerConfig;
 import com.vitamax.api.exception.GlobalControllerExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(GlobalControllerExceptionHandler.class)
+@Import({GlobalControllerExceptionHandler.class, SwaggerConfig.class})
 public class CourseCompositeServiceApplication {
 
     public static void main(String[] args) {
--- microservices/recommendation-service/src/main/java/com/vitamax/recommendation_service/RecommendationServiceApplication.java
+++ microservices/recommendation-service/src/main/java/com/vitamax/recommendation_service/RecommendationServiceApplication.java
@@ -1,5 +1,6 @@
 package com.vitamax.recommendation_service;
 
+import com.vitamax.api.config.SwaggerConfig;
 import com.vitamax.api.exception.GlobalControllerExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +9,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 
 @SpringBootApplication
 @EnableMongoRepositories
-@Import(GlobalControllerExceptionHandler.class)
+@Import({GlobalControllerExceptionHandler.class, SwaggerConfig.class})
 public class RecommendationServiceApplication {
 
     public static void main(String[] args) {
