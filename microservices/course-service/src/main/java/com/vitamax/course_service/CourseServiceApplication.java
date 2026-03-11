ced63b6bcf3e2ac8c750e0207843fa0d2ce4f8dd
Phuoc Nguyen <thanhphuoc01012001@gmail.com>
2026-03-11 22:11:36 +0700

    chore: each service should have their own config

--- microservices/course-service/src/main/java/com/vitamax/course_service/CourseServiceApplication.java
+++ microservices/course-service/src/main/java/com/vitamax/course_service/CourseServiceApplication.java
@@ -1,15 +1,11 @@
 package com.vitamax.course_service;
 
-import com.vitamax.api.config.SwaggerConfig;
-import com.vitamax.api.exception.GlobalControllerExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
-@Import({GlobalControllerExceptionHandler.class, SwaggerConfig.class})
 public class CourseServiceApplication {
 
     public static void main(String[] args) {
---
+++ microservices/course-service/src/main/java/com/vitamax/course_service/exception/CourseControllerExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.vitamax.course_service.exception;
+
+import com.vitamax.api.exception.GlobalControllerExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CourseControllerExceptionHandler extends GlobalControllerExceptionHandler {
+}
