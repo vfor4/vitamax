f64720b620677b97ba656971aa790a8be4c94065
Phuoc Nguyen <thanhphuoc01012001@gmail.com>
2026-04-08 22:06:45 +0700

    feat: use authorization code flow in swagger

--- microservices/review-service/src/main/java/com/vitamax/review_service/config/OpenApiConfig.java
+++ microservices/review-service/src/main/java/com/vitamax/review_service/config/OpenApiConfig.java
@@ -2,19 +2,28 @@ package com.vitamax.review_service.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.OAuthScope;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @OpenAPIDefinition(
-    security = @SecurityRequirement(name = "BearerAuth")
+        security = @SecurityRequirement(name = "OAuth2")
 )
 @SecurityScheme(
-    name = "BearerAuth",
-    type = SecuritySchemeType.HTTP,
-    scheme = "bearer",
-    bearerFormat = "JWT"
+        name = "OAuth2",
+        type = SecuritySchemeType.OAUTH2,
+        flows = @OAuthFlows(
+                authorizationCode = @OAuthFlow(
+                        authorizationUrl = "${app.auth.authorize-uri}",
+                        tokenUrl = "${app.auth.token-uri}",
+                        scopes = {
+                                @OAuthScope(name = "api:read", description = "API read permission"),
+                                @OAuthScope(name = "api:write", description = "API write permission")
+                        }
+                )
+        )
 )
 public class OpenApiConfig {
     // No @Bean needed — annotations do all the work
